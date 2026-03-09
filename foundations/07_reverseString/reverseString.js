acb026494614a7a6b660e5c3a776a374f51e6512
TalhaMaaz <talhamaaz@proton.me>
2026-03-09 06:32:20 +0500

    solve and test 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let i  = 0;
+  let finalArray = [];
+  let finalString = '';
+  let array  = string.split('')
+  for (i; i<string.length;i++){
+    finalArray.push(array.pop())
+  }
+  finalString = finalArray.join('')
+  return  finalString
 };
 
 // Do not edit below this line
