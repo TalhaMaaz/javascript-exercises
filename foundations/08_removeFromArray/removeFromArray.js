afba6ae6909fb8e7ac96338761151f3317ff1db7
TalhaMaaz <talhamaaz@proton.me>
2026-03-09 07:13:07 +0500

    solve and test 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...rest) {
+  let finalArray;
+  finalArray = array.filter((x) => !rest.includes(x));
+  return finalArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
