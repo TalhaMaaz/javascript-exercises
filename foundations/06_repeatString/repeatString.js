f9a8c03303bbfb995c8df15c3adb3db3b2d774a5
TalhaMaaz <talhamaaz@proton.me>
2026-03-09 06:17:50 +0500

    solve 06 v1

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num <0){
+    return 'ERROR';
+  }
+  else{
+    let i = 0;
+    let finalString='';
+    for (i; i<num ; i++){
+      finalString +=string
+    }
+    return finalString
+  }
 };
 
 // Do not edit below this line
