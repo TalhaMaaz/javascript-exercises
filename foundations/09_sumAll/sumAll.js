41d3cb44dcea5259fb67836b7d2e0b85a1a8570f
TalhaMaaz <talhamaaz@proton.me>
2026-03-09 07:51:46 +0500

    solve and test 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (!Number.isInteger(start) || !Number.isInteger(end) || start <0 || end<0){
+    return 'ERROR'
+  }
+  else{
 
+  let min = Math.min(start,end);
+  let max = Math.max(end,start);
+  let array = [];
+  for (min;min<=max;min++){
+    array.push(min)
+  }
+  //console.log(array)
+
+  let sum = array.reduce((total, current) => {
+    total += current;
+    return total;
+  }, 0);
+  return sum;
+  }
 };
+
+console.log(sumAll(1,3))
 
 // Do not edit below this line
 module.exports = sumAll;
